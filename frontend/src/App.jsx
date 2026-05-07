d6208c0fad18ffec9e68edcaa899b90b592f9812
Themiya <themiyakaluarachchi@gmail.com>
2026-05-07 14:48:54 +0530

    Add CategoriesPage component and Axios API setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,10 @@
 import './App.css'
+import CategoriesPage from './pages/CategoriesPage'
 
 function App() {
 
   return (
-    <>
-      <div className='bg-green-500'>
-        <h1 className='text-3xl font-bold underline text-center text-white'>Hello world!</h1>
-      </div>
-    </>
+    <CategoriesPage />
   )
 }
 
