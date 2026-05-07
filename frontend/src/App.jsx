42540819b56f4f1a3461c77650b4852f12c8acc5
Themiya <themiyakaluarachchi@gmail.com>
2026-05-07 12:37:39 +0530

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='bg-green-500'>
+        <h1 className='text-3xl font-bold underline text-center text-white'>Hello world!</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
